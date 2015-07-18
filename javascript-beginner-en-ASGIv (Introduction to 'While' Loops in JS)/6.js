2feec51503d0a62918b4e19c99dc1f247380611c
Josh <linkingparents@gmail.com>
2015-07-18 13:16:20 -0300

    6/11

---
+++ javascript-beginner-en-ASGIv (Introduction to 'While' Loops in JS)/6.js
@@ -0,0 +1,13 @@
+//Remember to make your condition true outside the loop!
+
+var soloLoop = function(){
+  //Your code goes here!
+  var condition = true;
+  
+  while(condition) {
+    console.log("Looped once!");
+    condition = false;
+  }
+}
+
+soloLoop();
