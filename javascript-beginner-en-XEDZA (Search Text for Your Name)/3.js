6e472660bbe8d72d9f4f643dbe99add209166d2e
Josh <linkingparents@gmail.com>
2015-07-18 13:03:39 -0300

    3/7

---
+++ javascript-beginner-en-XEDZA (Search Text for Your Name)/3.js
@@ -0,0 +1,10 @@
+/*jshint multistr:true */
+
+var text = "ewuihfq John awidjeiuh Josh yesterday we went shopping in roadrunnerville, john jacob Josh.";
+
+var myName = "Josh";
+var hits = [ ];
+
+for (var i = 0; i < text.length; i++) {
+    console.log(text[i]);
+}
