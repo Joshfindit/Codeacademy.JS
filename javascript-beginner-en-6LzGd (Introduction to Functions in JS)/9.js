570400b43f23756a0ffe752c43d19fa576957ef6
Josh <linkingparents@gmail.com>
2015-07-18 12:40:15 -0300

    9/13

---
+++ javascript-beginner-en-6LzGd (Introduction to Functions in JS)/9.js
@@ -0,0 +1,8 @@
+// Write your function starting on line 3
+
+var perimeterBox = function (length, width) {
+    var perimeter = length + length + width + width;
+    return perimeter;
+};
+
+console.log(perimeterBox(6, 9));
