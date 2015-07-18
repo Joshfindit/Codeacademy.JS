85b39302bf6657e6c4b70fbb0ff4c00c2d266df1
Josh <linkingparents@gmail.com>
2015-07-18 13:36:24 -0300

    11/11

---
+++ javascript-beginner-en-ASGIv (Introduction to 'While' Loops in JS)/11.js
@@ -0,0 +1,16 @@
+// Write your code below!
+var string = "Here is a string of some sort";
+var kidsNotInRoom = 0;
+
+for(var i=0; i < 25; i++) {
+    console.log(i);
+}
+
+do {
+    console.log("Be silly, stop coding");
+} while (kidsNotInRoom);
+
+while ( i < string.length) {
+    console.log("While loop: " + string[i]);
+    i += 1;
+}
