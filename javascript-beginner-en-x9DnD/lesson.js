c9f9c1ff884f9f5515c0ab1da8ef23f7aaa62145
Josh <linkingparents@gmail.com>
2015-07-18 12:03:10 -0300

    4/7

--- javascript-beginner-en-x9DnD/lesson.js
+++ javascript-beginner-en-x9DnD/lesson.js
@@ -8,9 +8,12 @@ if (age >= 13) {
     console.log("Play on!");
 }
 else {
-    console.log("You're too young, go away!");
+    console.log("You're too young for Bieber. Go away!");
 }
 
 var scene1 = "You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'";
+var beiberQuestion1 = "Suddenly, Bieber stops and says, 'Who wants to race me?'"
 
 console.log(scene1);
+console.log(beiberQuestion1);
+var userAnswer = prompt ("Do you want to race Bieber on stage?");
