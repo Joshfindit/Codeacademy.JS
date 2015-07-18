90caf04b22baa9477b92dd5dccfd59532db157f5
Josh <linkingparents@gmail.com>
2015-07-18 12:49:29 -0300

    4/9

--- javascript-beginner-en-Bthev-mskY8 (Build Rock, Paper, Scissors)/RockPaperScissors.js
+++ javascript-beginner-en-Bthev-mskY8 (Build Rock, Paper, Scissors)/RockPaperScissors.js
@@ -2,3 +2,20 @@ userChoice = prompt("Do you choose rock, paper or scissors?");
 
 computerChoice = Math.random();
 console.log(computerChoice);
+
+if (computerChoice < 0.33 ) {
+    computerChoice = "rock";
+}
+else if (0.33 < computerChoice && computerChoice < 0.66 )
+{
+    computerChoice = "paper";
+}
+else if ( computerChoice > 0.66 )
+{
+    computerChoice = "scissors";
+}
+else
+{
+}
+
+console.log(computerChoice);
