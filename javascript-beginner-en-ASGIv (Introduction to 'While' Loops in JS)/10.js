6bb26cd0a24915264d28a7593323ac939bde0279
Josh <linkingparents@gmail.com>
2015-07-18 13:21:26 -0300

    10/11

---
+++ javascript-beginner-en-ASGIv (Introduction to 'While' Loops in JS)/10.js
@@ -0,0 +1,9 @@
+var getToDaChoppa = function(condition){
+  // Write your do/while loop here!
+  do {
+      console.log("String; user choice");
+      condition -= 1;
+  } while (condition);
+};
+
+getToDaChoppa(3);
