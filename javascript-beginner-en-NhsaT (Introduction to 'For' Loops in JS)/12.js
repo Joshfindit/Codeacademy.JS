a00d2fb329cafec2d749de382107dd831bd4e739
Josh <linkingparents@gmail.com>
2015-07-18 13:01:35 -0300

    12/13

---
+++ javascript-beginner-en-NhsaT (Introduction to 'For' Loops in JS)/12.js
@@ -0,0 +1,7 @@
+// Click on "Stuck? Get a hint!" if you get stuck!
+
+var names = [ "Joe", "Shaq", "Peter", "Why", "Yesterday" ];
+
+for(var i= 0; i < names.length; i++) {
+    console.log("I know someone called " + names[i]);
+}
