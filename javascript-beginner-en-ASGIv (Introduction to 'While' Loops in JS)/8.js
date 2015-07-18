5b2e03b7764de91a559dd48fb0f2505546f643d7
Josh <linkingparents@gmail.com>
2015-07-18 13:16:30 -0300

    8/11

---
+++ javascript-beginner-en-ASGIv (Introduction to 'While' Loops in JS)/8.js
@@ -0,0 +1,13 @@
+var numberOfStarsEstimated = 1737;
+
+while(numberOfStarsEstimated >=0) {
+    contemplateExistence();
+    numberOfStarsEstimated -= 1;
+}
+    
+for(i = 0; i < (Math.random() *100); i++) {
+    console.log("i: " + i);
+}
+    
+var contemplateExistence = function() {   
+}
