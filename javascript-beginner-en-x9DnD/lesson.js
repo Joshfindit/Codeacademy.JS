9db92a5f8270a2025bf4da791dac7d790dff4c29
Josh <linkingparents@gmail.com>
2015-07-18 12:01:09 -0300

    2/7

---
+++ javascript-beginner-en-x9DnD/lesson.js
@@ -0,0 +1,15 @@
+// Check if the user is ready to play!
+
+confirm("Press OK when you are ready!");
+
+var age = prompt("What's your age?");
+
+if (age >= 13) {
+    console.log("Play on!");
+}
+else {
+    console.log("You're too young, go away!");
+}
+
+
+
