3cfbede75b8d9213920c20e3eb23c7bb011b8548
Josh <linkingparents@gmail.com>
2015-07-18 13:07:30 -0300

    5/11

---
+++ javascript-beginner-en-ASGIv (Introduction to 'While' Loops in JS)/5.js
@@ -0,0 +1,12 @@
+//Remember to set your condition outside the loop!
+
+
+var loop = function(number, string){
+	while(number != 0){
+		//Your code goes here!
+		console.log(string);
+		number -= 1;
+	}
+};
+
+loop(3, "I'm looping!");
