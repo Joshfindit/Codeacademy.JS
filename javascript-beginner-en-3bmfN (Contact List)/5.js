ff0a19e44b12cbff217111ac7e142bb476d8e0d7
Josh <linkingparents@gmail.com>
2015-07-20 13:07:54 -0300

    5/8

---
+++ javascript-beginner-en-3bmfN (Contact List)/5.js
@@ -0,0 +1,17 @@
+var friends = new Object();
+
+friends.bill = {
+    firstName: "Bill",
+    lastName: "Franks",
+    number: 53,
+    address: [ '2601 Mission St #5TH', 'San Francisco', 'CA', '94110' ],
+}
+
+friends.steve = {
+    firstName: "Steve",
+    lastName: "Guttenburg",
+    number: 8,
+    address: [ '8942 Wilshire Blvd', 'Beverly Hills', 'CA', '90211' ],
+}
+
+
