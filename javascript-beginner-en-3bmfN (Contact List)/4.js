83d179f1fdaa49ddbbf538f1fea15b67b48a7993
Josh <linkingparents@gmail.com>
2015-07-20 13:02:42 -0300

    4/8

---
+++ javascript-beginner-en-3bmfN (Contact List)/4.js
@@ -0,0 +1,13 @@
+var friends = new Object();
+
+friends.bill = {
+    firstName: "Bill",
+    lastName: "Franks",
+    number: 53
+}
+
+friends.steve = {
+    firstName: "Steve",
+    lastName: "Gutenburg",
+    number: 8
+}
