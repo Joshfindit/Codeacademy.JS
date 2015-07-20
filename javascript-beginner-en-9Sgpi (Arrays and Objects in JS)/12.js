723d7b7c944ac0ceeba0757d415fdf9cc4fb4c0d
Josh <linkingparents@gmail.com>
2015-07-20 11:50:48 -0300

    12/17

---
+++ javascript-beginner-en-9Sgpi (Arrays and Objects in JS)/12.js
@@ -0,0 +1,13 @@
+var object1 = {
+    name: "Thing1",
+    colour: "Red"
+}
+
+var object2 = {
+    name: "Thing2",
+    colour: "Peridot"
+}
+
+var object3 = new Object();
+
+object3.type = "Table";
