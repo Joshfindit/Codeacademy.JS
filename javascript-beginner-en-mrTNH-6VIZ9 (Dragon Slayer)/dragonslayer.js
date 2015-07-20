fceaf8820afdec8ad598fd271fbee152452ca2f8
Josh <linkingparents@gmail.com>
2015-07-20 09:56:21 -0300

    4/6

--- javascript-beginner-en-mrTNH-6VIZ9 (Dragon Slayer)/dragonslayer.js
+++ javascript-beginner-en-mrTNH-6VIZ9 (Dragon Slayer)/dragonslayer.js
@@ -4,5 +4,12 @@ var damageThisRound = Math.floor(Math.random()*5 + 1);
 var totalDamage = 0;
 
 while(slaying) {
+    if (youHit) {
+        console.log("You hit the dragon!");
+    } else {
+        console.log("You swing at the dragon and miss. Your momentum carries you right in to the path of it's claws and you are no more.");
+        slaying = false;
+    }
+    
     slaying = false;
 }
