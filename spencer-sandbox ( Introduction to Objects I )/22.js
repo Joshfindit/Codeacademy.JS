dadad551509c20dea0531dd61c5693284b19b66f
Josh <linkingparents@gmail.com>
2015-07-21 14:59:13 -0300

    22/33

---
+++ spencer-sandbox ( Introduction to Objects I )/22.js
@@ -0,0 +1,12 @@
+function Cat(age, colour) {
+  this.age = age;
+  this.colour = colour;
+}
+
+// make a Dog constructor here
+function Dog(age, name, breed, colour) {
+  this.age = age;
+  this.name = name;
+  this.breed = breed;
+  this.colour = colour;
+}
